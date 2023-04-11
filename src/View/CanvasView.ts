ec6ef26f8747628870e16dff755ae8a99c875eb2
Craig Wilson <cw66786@gmail.com>
2023-04-10 19:06:04 -0500

    savepoint to restart vs code

--- src/View/CanvasView.ts
+++ src/View/CanvasView.ts
@@ -4,5 +4,17 @@ import { Ball } from '../sprites/Ball';
 
 
 export class CanvasView {
-    
+    canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D | null;
+    private scoreDisplay: HTMLObjectElement | null;
+    private startButton: HTMLObjectElement | null;
+    private info: HTMLObjectElement | null;
+
+
+
+    constructor(canvasName: string){
+        this.canvas = document.querySelector(canvasName) as HTMLCanvasElement;
+        
+    }
+   
 }
